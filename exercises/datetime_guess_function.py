4b6d5bca784e77eeb079e175bf318cd7aea0a3a9
amandabz <amandabenitezhidalgo@gmail.com>
2023-10-19 12:19:28 +0200

    name replacement in guess and datetime function

--- exercises/datetime_guess_function.py
+++ exercises/datetime_guess_function.py
@@ -2,7 +2,7 @@ import random
 import datetime
 
 
-def cold_and_hot_number():
+def guess_function_with_attemps_and_time():
     random_value = random.randrange(1, 100)
     print(random_value)
 
@@ -11,24 +11,29 @@ def cold_and_hot_number():
     start_time = datetime.datetime.now()
 
     while random_value != number:
+
+        # attempts used
         number = int(input("Introduce un número entre el 1 y el 100: "))
         attempts += 1
+        number_difference = abs(number - random_value)
+
+        # time spend
         end_time = datetime.datetime.now()
         time_difference = end_time - start_time
         formatted_time = str(time_difference).split(".")[0]
 
-        if number > 100:
-            print("Has introducido un número mayor al rango establecido. Introduce un número entre el 1 y el 100: ")
-
-        elif number == random_value:
+        if number == random_value:
             print(f"¡Acertaste en {attempts} intentos y has tardado {formatted_time}!")
             break
 
-        elif abs(number - random_value) <= 10:
-            print("Caliente...")
-        else:
+        elif number > 100:
+            print("Has introducido un número mayor al rango establecido. Introduce un número entre el 1 y el 100: ")
 
+        elif number_difference <= 10:
+            print("Caliente...")
+
+        else:
             print("Frío...")
 
 
-cold_and_hot_number()
+guess_function_with_attemps_and_time()
