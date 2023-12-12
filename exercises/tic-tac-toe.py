d6177e3237d75be5d6c193678efa41a2adf4c753
amandabz <amandabenitezhidalgo@gmail.com>
2023-12-12 13:45:21 +0100

    tic tac toe game with numeric board

--- exercises/tic-tac-toe.py
+++ exercises/tic-tac-toe.py
@@ -1,10 +1,12 @@
 import random
 
 board = [
-    ["█", "█", "█"],
-    ["█", "█", "█"],
-    ["█", "█", "█"]
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
 ]
+
+board_positions_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def print_board():
@@ -13,7 +15,7 @@ def print_board():
 
 
 def draw_game():
-    if all(position != "█" for row in board for position in row):
+    if all(position not in board_positions_list for row in board for position in row):
         return True
     return False
 
@@ -40,7 +42,7 @@ def check_winner(current_player):
 
 def computer_move():
     available_positions = [(index, j) for index, row in enumerate(board)
-                           for j, position in enumerate(row) if position == "█"]
+                           for j, position in enumerate(row) if position in board_positions_list]
 
     if available_positions:
         return random.choice(available_positions)
@@ -48,45 +50,12 @@ def computer_move():
 
 
 def main():
-    current_player = "O"
+    current_player = "X"
 
     while True:
         print_board()
 
-        if current_player == "O":
-            try:
-                movement = int(input(f"Player O, enter a position (0-8): "))
-
-                if movement <= 8:
-                    row = movement // 3
-                    col = movement % 3
-
-                    if board[row][col] == "█":
-                        board[row][col] = "O"
-
-                        if check_winner("O"):
-                            print_board()
-                            print("Player with O icon wins!")
-                            break
-
-                        elif draw_game():
-                            print("We have a draw!")
-                            break
-
-                    else:
-                        print("Cell already taken or invalid. Try again.")
-                        continue
-
-                    current_player = "X" if current_player == "O" else "O"
-
-                else:
-                    print("Please enter a value between 0 and 8")
-                    continue
-
-            except ValueError:
-                print("Please, enter a number between 0 and 8")
-
-        else:
+        if current_player == "X":
             print("Computer's move:")
             computer_position = computer_move()
 
@@ -100,10 +69,46 @@ def main():
                     break
 
                 elif draw_game():
+                    print_board()
                     print("We have a draw!")
                     break
 
                 current_player = "O"
+
+        else:
+            try:
+                movement = int(input(f"Player O, enter a position (0-9): "))
+
+                if movement <= 9:
+                    movement -= 1  # adjustment to start the table index from the number 1
+                    row = movement // 3
+                    col = movement % 3
+
+                    if board[row][col] in board_positions_list:
+                        board[row][col] = "O"
+
+                        if check_winner("O"):
+                            print_board()
+                            print("Player with O icon wins!")
+                            break
+
+                        elif draw_game():
+                            print_board()
+                            print("We have a draw!")
+                            break
+
+                    else:
+                        print("Cell already taken or invalid. Try again.")
+                        continue
+
+                    current_player = "X" if current_player == "O" else "O"
+
+                else:
+                    print("Please enter a value between 1 and 9")
+                    continue
+
+            except ValueError:
+                print("Please, enter a number between 1 and 9")
 
 
 if __name__ == "__main__":
